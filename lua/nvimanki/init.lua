801a2f81d73603cec10c52655e1be535b53dbd28
Rase <rasengithub@protonmail.com>
2022-06-13 16:09:05 +0300

    New plugin repository.
    My goals for this project are a clean git history, a good and well
    working plugin.

---
+++ lua/nvimanki/init.lua
@@ -0,0 +1,13 @@
+-- Neovim global plugin that uploads notes into Anki.
+-- Last Change:	2022 Jun 13
+-- Maintainer:	Rasmus Mäki <rasengithub@protonmail.com>
+-- License:	This file is placed in the public domain.
+local utils = require('nvimanki.utils')
+
+local G = {}
+
+function G.testimodule()
+  print("testipaskaa")
+end
+
+return G
---
+++ lua/nvimanki/utils.lua
@@ -0,0 +1,23 @@
+local G = {}
+
+-- Returns the text contents of the visualized text.
+G.visual_selection = function()
+  -- does not handle rectangular selection
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  if lines[1] ~= nil then
+    lines[1] = string.sub(lines[1], s_start[3], -1)
+    if n_lines == 1 then
+      lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+    else
+      lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+    end
+    return lines
+  else
+    return ""
+  end
+end
+
+return G
