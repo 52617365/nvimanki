2e574840e8f594ccd46c6e5b91de32734c8ab444
Rase <rasengithub@protonmail.com>
2022-06-16 09:00:33 +0300

    Init.lua now calls code from separated module.
    Did the refactor so had to refactor main too to call the code.

--- lua/nvimanki/init.lua
+++ lua/nvimanki/init.lua
@@ -2,13 +2,13 @@
 -- Last Change:	2022 Jun 13
 -- Maintainer:	Rasmus Mäki <rasengithub@protonmail.com>
 -- License:	This file is placed in the public domain.
-local utils = require('nvimanki.utils')
+local utils = require('nvimanki.api')
 
 local G = {}
 
 function G.testimodule()
   local decks = utils.get_all_decks()
-  print(vim.inspect(decks))
+  -- print(vim.inspect(decks))
 end
 
 return G
