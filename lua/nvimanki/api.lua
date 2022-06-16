dc6e5290c031b4c0e8750ac0d51b6d1b150c52da
Rase <rasengithub@protonmail.com>
2022-06-16 08:58:02 +0300

    API code separated to its own module and file.
    All of the code interacting with the AnkiConnect API will be going into
    the api.lua file.
    
    Currently only getting the decks is working, sending a payload and
    adding decks is not yet supported.

---
+++ lua/nvimanki/api.lua
@@ -0,0 +1,56 @@
+local curl = require "plenary.curl"
+local M = require "nvimanki.fileio"
+local G = {}
+
+-- Gets all decks from the AnkiConnect endpoint and then writes them all into a file.
+G.get_all_decks = function()
+  local body = {
+    action = "deckNames",
+    version = 7,
+  }
+
+  local res = curl.post("127.0.0.1:8765", {
+    body = vim.fn.json_encode(body),
+    headers = {
+      content_type = "application/json",
+    },
+  })
+
+  if res.status == 200 then
+    local parsed_response = vim.fn.json_decode(res.body)
+    if parsed_response.error ~= vim.NIL then
+      print("server not running")
+      return nil
+    else
+      M.write_to_file(parsed_response.result)
+      return 0
+    end
+  else
+    print("error sending request")
+    return nil
+  end
+end
+
+-- This is not yet used.
+G.send_request = function(query)
+  local body = {
+    action = "deckNames",
+    version = 7,
+  }
+
+  local res = curl.post("127.0.0.1:8765", {
+    body = vim.fn.json_encode(body),
+    headers = {
+      content_type = "application/json",
+    },
+  })
+
+  if res.status == 200 then
+    return res.body
+  else
+    print("Server not running.")
+    return nil
+  end
+end
+
+return G
