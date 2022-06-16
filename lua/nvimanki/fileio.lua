ff832308f7c2125ad83d5ba2514d39b0e013104b
Rase <rasengithub@protonmail.com>
2022-06-16 11:07:34 +0300

    Function that reads a files contents ready
    It gets the test file, reads the contents into a table.

--- lua/nvimanki/fileio.lua
+++ lua/nvimanki/fileio.lua
@@ -15,4 +15,24 @@ G.write_to_file = function(contents)
   end
 end
 
+
+-- see if the file exists
+local function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- Reads file, returns an empty table if file does not exist.
+G.read_deck_file = function()
+  local file = "testfile.txt"
+  if not file_exists(file) then
+    return {}
+  end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
 return G
