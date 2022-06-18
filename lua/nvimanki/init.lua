f591d02f41ab048f5fa4e17e68dcfeca6c6daa97
Rase <rasengithub@protonmail.com>
2022-06-18 15:55:25 +0300

    testing visual selection.

--- lua/nvimanki/init.lua
+++ lua/nvimanki/init.lua
@@ -1,9 +1,10 @@
 -- Neovim global plugin that uploads notes into Anki.
--- Last Change:	2022 Jun 13
+-- Last Change:	2022 Jun 18
 -- Maintainer:	Rasmus Mäki <https://github.com/52617365/>
 -- License:	This file is placed in the public domain.
 local ui = require('nvimanki.picker')
 local api = require('nvimanki.api')
+local utils = require('nvimanki.utils')
 
 -- TODO:
 -- UI to delete done when api starts working.
@@ -11,6 +12,19 @@ local api = require('nvimanki.api')
 
 -- public interface starts here.
 local G = {}
+
+-- format is |question| - |answer|
+function G.create_deck_from_visualization()
+  local visualized_text = utils.visual_selection()
+  local dash = string.find(visualized_text, "|-|")
+  if dash == nil then
+    print("Incorrect form.")
+  else
+    local question = visualized_text.sub(0, dash)
+    local answer = visualized_text.sub(dash + 1, #visualized_text)
+    api.choose_deck(question, answer)
+  end
+end
 
 function G.create_note()
   ui.make_a_note()
