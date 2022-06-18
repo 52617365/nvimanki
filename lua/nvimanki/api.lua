1acd3ead6d14991426065e503222126774b3fd8b
Rase <rasengithub@protonmail.com>
2022-06-18 11:39:27 +0300

    Update decks works again desc for more.
    I separated the error handling logic from the post request method. It is
    way clearer this way and I got the parsing of the response body working
    now so it updates the file correctly.

--- lua/nvimanki/api.lua
+++ lua/nvimanki/api.lua
@@ -6,23 +6,25 @@ local G = {}
 -- Update the file containing decks everytime a new deck gets created or a deck gets deleted.
 -- Prompt user to update decks if they can't be found.
 
+-- nil if err, res body if not.
 G.api_error_handling = function(res, success, error)
   if res.status == 200 then
     local parsed_response = vim.fn.json_decode(res.body)
     if parsed_response.error ~= vim.NIL then
       print(error)
-      return 1
+      return nil
     else
       print(success)
-      return 0
+      return parsed_response.result
     end
   else
     print(error)
-    return 1
+    return nil
   end
 end
 
-G.send_post_request = function(body, success, error)
+-- TODO: check for nil when calling this method to handle error.
+G.send_post_request = function(body)
   local res = curl.post("127.0.0.1:8765", {
     body = vim.fn.json_encode(body),
     headers = {
@@ -31,12 +33,7 @@ G.send_post_request = function(body, success, error)
   })
 
   -- Return nil if error.
-  local err = G.api_error_handling(res, success, error)
-  if err == 1 then
-    return err
-  else
-    return res
-  end
+  return res
 end
 
 -- Gets all decks from the AnkiConnect endpoint and then writes them all into a file.
@@ -45,11 +42,12 @@ G.update_decks = function()
     action = "deckNames",
     version = 7,
   }
-  local res = G.send_post_request(body, "succesfully updated decks", "error updating decks")
-
+  local res = G.send_post_request(body)
+  local err = G.api_error_handling(res, "succesfully updated decks", "error updating decks")
   -- if it was not an error, write to file.
-  if res ~= 1 then
-    M.write_to_file(res.result)
+  if err ~= nil then
+    -- TODO: check that res.result is not nil
+    M.write_to_file(err)
   end
 end
 
@@ -77,7 +75,10 @@ G.create_note = function(deck, question, answer)
       }
     }
   }
-  local _ = G.send_post_request(body, "succesfully created note", "error creating note")
+  local res = G.send_post_request(body)
+
+  -- We don't care about result value, it will print err if it runs into one.
+  local _ = G.api_error_handling(res, "succesfully created note", "error creating note")
 end
 
 
@@ -89,9 +90,12 @@ G.create_deck = function(deck_name)
     params = { deck = deck_name },
   }
 
-  local res = G.send_post_request(body, "succesfully created " .. deck_name, "error creating " .. deck_name)
+  local res = G.send_post_request(body)
 
-  if res == 0 then
+  local err = G.api_error_handling(res, "succesfully created " .. deck_name, "error creating " .. deck_name)
+
+  -- nil if error.
+  if err ~= nil then
     G.update_decks()
   end
 end
@@ -110,10 +114,11 @@ G.delete_deck = function()
   }
   print(vim.fn.json_encode(body))
 
-  local res = G.send_post_request(body, "succesfully deleted " .. deck_name, "error deleting " .. deck_name)
+  local res = G.send_post_request(body)
+  local err = G.api_error_handling(res, "succesfully deleted " .. deck_name, "error deleting " .. deck_name)
 
   -- if success, update decks.
-  if res == 0 then
+  if err ~= nil then
     G.update_decks()
   end
 end
