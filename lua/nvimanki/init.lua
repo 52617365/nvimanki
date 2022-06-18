3205736249f334959c42159d193e736d2b6b44f5
Rase <rasengithub@protonmail.com>
2022-06-18 11:41:18 +0300

    Renamed function so that it fits Anki terminology.

--- lua/nvimanki/init.lua
+++ lua/nvimanki/init.lua
@@ -3,12 +3,13 @@
 -- Maintainer:	Rasmus Mäki <rasengithub@protonmail.com>
 -- License:	This file is placed in the public domain.
 local ui = require('nvimanki.picker')
--- local api = require('nvimanki.api')
+local api = require('nvimanki.api')
 
 local G = {}
 
 function G.testimodule()
-  ui.make_note()
+  -- ui.make_question_card()
+  api.update_decks()
 end
 
 return G
